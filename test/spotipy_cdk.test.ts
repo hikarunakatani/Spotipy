53ff3622e410f3f52c549ecc4d1b57ce03246ce4
hikarunakatani <hikamichael@hotmail.co.jp>
2022-12-02 22:05:28 +0900

    Initial commit

---
+++ test/spotipy_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SpotipyCdk from '../lib/spotipy_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/spotipy_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SpotipyCdk.SpotipyCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
